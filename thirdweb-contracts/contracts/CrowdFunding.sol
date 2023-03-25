5f102fe6a44a565bfb9f6d4d694703f3651ebc55
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-03-26 03:37:49 +0530

    Donate to a Campaign

--- thirdweb-contracts/contracts/CrowdFunding.sol
+++ thirdweb-contracts/contracts/CrowdFunding.sol
@@ -37,7 +37,20 @@ contract CrowdFunding {
     }
 
 
-    function donateToCampaign() {}
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+
+        if(sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
 
     function getDonators() {}
 
