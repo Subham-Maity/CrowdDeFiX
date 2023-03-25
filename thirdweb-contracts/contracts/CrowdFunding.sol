2034f92c2d6999638da6cc9c69aa42d1cbdf9d91
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-03-26 03:10:04 +0530

    Set the values of each property of the Campaign struct
     1. createCampaign() which takes six arguments: _owner, _title, _description, _target, _deadline, and _image.
    
    2. deadline check
    
    3. Set the values of each property of the Campaign struct
    
    4. increment the value of numberOfCampaigns and return its value minus one.

--- thirdweb-contracts/contracts/CrowdFunding.sol
+++ thirdweb-contracts/contracts/CrowdFunding.sol
@@ -21,6 +21,21 @@ contract CrowdFunding {
      function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
         Campaign storage campaign = campaigns[numberOfCampaigns];
 
+        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
+    }
+
 
     function donateToCampaign() {}
 
