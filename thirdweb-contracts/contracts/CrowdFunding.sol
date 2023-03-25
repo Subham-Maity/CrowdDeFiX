fc7596c45b7b9fe45ad1a5f2d443bfd8c30dda63
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-03-26 04:07:35 +0530

    Get Donators

--- thirdweb-contracts/contracts/CrowdFunding.sol
+++ thirdweb-contracts/contracts/CrowdFunding.sol
@@ -52,7 +52,9 @@ contract CrowdFunding {
         }
     }
 
-    function getDonators() {}
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
 
     function getCampaigns() {}
 }
