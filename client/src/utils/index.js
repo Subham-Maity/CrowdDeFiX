b2628317d8a7cdd3177f9b943bf83b7e4c5bac66
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-03-31 06:13:22 +0530

    Utils-> How much money has been raised so far

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -7,3 +7,10 @@ export const daysLeft = (deadline) => {
   return remainingDays.toFixed(0);
 };
 
+//calculate the percentage of the goal that has been reached
+//How much money has been raised so far
+export const calculateBarPercentage = (goal, raisedAmount) => {
+  const percentage = Math.round((raisedAmount * 100) / goal);
+
+  return percentage;
+};
