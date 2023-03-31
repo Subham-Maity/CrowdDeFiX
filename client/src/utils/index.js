0e3ed552ed0930384d39ebba369556c88bebe537
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-03-31 06:13:02 +0530

    Utils-> Calculate the days left from the date calculating

---
+++ client/src/utils/index.js
@@ -0,0 +1,9 @@
+//calculate the days left from the date calculating
+
+export const daysLeft = (deadline) => {
+  const difference = new Date(deadline).getTime() - Date.now();
+  const remainingDays = difference / (1000 * 3600 * 24);
+
+  return remainingDays.toFixed(0);
+};
+
